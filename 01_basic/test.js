574e56d71174151e7de9897d4122f2c3c4a6f954
kashif5500 <145431430+kashif5500@users.noreply.github.com>
2024-05-11 06:55:48 +0000

    ok

--- 01_basic/test.js
+++ 01_basic/test.js
@@ -1,2 +1,17 @@
-account Id="12345"
-account password="987"
+const accountId=12345
+let accountEmail="kashif@gmail.com"
+var accountPassword ="12345"
+accountCity="jaipur"
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+accountEmail="mdanam@gmail.com"
+accountPassword="4444"
+accountCity="ghazipur"
+
+console.log(accountId);
+
+console.table([accountCity,accountPassword,accountEmail])
+

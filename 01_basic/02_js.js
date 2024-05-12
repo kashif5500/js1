6067a6781f12735835fdd574b04a21d38a3438d9
kashif5500 <145431430+kashif5500@users.noreply.github.com>
2024-05-12 20:22:29 +0000

    ok

--- 01_basic/02_js.js
+++ 01_basic/02_js.js
@@ -1,16 +1,23 @@
-"use strict";
-// alert(3+3)//we are using nodejs,not browser
+// "use strict";
+// // alert(3+3)//we are using nodejs,not browser
 
-console.log(3 + 3)
-console.log("kashif")
+// console.log(3 + 3)
+// console.log("kashif")
 
-console.log(typeof null)
+// console.log(typeof null)
 
-let score= true
+// let score= true
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-let valueInnumber=Number(score)
-console.log(typeof valueInnumber)
-console.log(valueInnumber)
+// let valueInnumber=Number(score)
+// console.log(typeof valueInnumber)
+// console.log(valueInnumber)
+
+//***************    Operations    *******************//
+
+let value =3
+let negValue =-value
+// console.log(negValue)
+console.log("3"+(1+2))

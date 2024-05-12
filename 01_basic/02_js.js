be7a0ba257e965af8960879dea2c15d596585810
kashif5500 <145431430+kashif5500@users.noreply.github.com>
2024-05-12 19:35:54 +0000

    ok

---
+++ 01_basic/02_js.js
@@ -0,0 +1,16 @@
+"use strict";
+// alert(3+3)//we are using nodejs,not browser
+
+console.log(3 + 3)
+console.log("kashif")
+
+console.log(typeof null)
+
+let score= true
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInnumber=Number(score)
+console.log(typeof valueInnumber)
+console.log(valueInnumber)

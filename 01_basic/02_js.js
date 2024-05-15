02374b3edcb69b1c39b845816513b2d089bdf2f1
kashif5500 <145431430+kashif5500@users.noreply.github.com>
2024-05-15 06:08:19 +0000

    ok

--- 01_basic/02_js.js
+++ 01_basic/02_js.js
@@ -1,23 +1,36 @@
-// "use strict";
-// // alert(3+3)//we are using nodejs,not browser
+// // "use strict";
+// // // alert(3+3)//we are using nodejs,not browser
 
-// console.log(3 + 3)
-// console.log("kashif")
+// // console.log(3 + 3)
+// // console.log("kashif")
 
-// console.log(typeof null)
+// // console.log(typeof null)
 
-// let score= true
+// // let score= true
 
-// console.log(typeof score)
-// console.log(typeof(score))
+// // console.log(typeof score)
+// // console.log(typeof(score))
 
-// let valueInnumber=Number(score)
-// console.log(typeof valueInnumber)
-// console.log(valueInnumber)
+// // let valueInnumber=Number(score)
+// // console.log(typeof valueInnumber)
+// // console.log(valueInnumber)
 
-//***************    Operations    *******************//
+// //***************    Operations    *******************//
 
-let value =3
-let negValue =-value
-// console.log(negValue)
-console.log("3"+(1+2))
+// let value =3
+// let negValue =-value
+// // console.log(negValue)
+// // console.log("3"+(1+2))
+
+// x=5
+// x++
+// console.log(x)
+
+   const gameName=new String('global')
+
+   console.log(gameName[0])
+   console.log(gameName._proto_)
+   console.log(gameName.length)
+   console.log (gameName.charAt(2))
+   console.log(gameName.indexOf('o'))
+   console.log(gameName.slice(-4,3))

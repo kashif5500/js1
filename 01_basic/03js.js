92b6f9126f97d6cbe35fcfd461cfa6a0027a0deb
kashif5500 <145431430+kashif5500@users.noreply.github.com>
2024-05-15 19:21:32 +0000

    k

--- 01_basic/03js.js
+++ 01_basic/03js.js
@@ -1,35 +1,50 @@
-// // const score = 400
+// // // const score = 400
 
-// // const balance = new Number(100)
-// // console.log(score);
+// // // const balance = new Number(100)
+// // // console.log(score);
 
-// // console.log(balance);
+// // // console.log(balance);
 
-// // console.log(balance.toString().length);
+// // // console.log(balance.toString().length);
 
-// // console.log(balance.toFixed(1));
+// // // console.log(balance.toFixed(1));
 
-// // const otherNumber =2667.87665
+// // // const otherNumber =2667.87665
 
-// // console.log(otherNumber.toPrecision(3));
+// // // console.log(otherNumber.toPrecision(3));
 
-// // const hundreds=10000000
-// // console.log(hundreds.toLocaleString('en-IN'))
+// // // const hundreds=10000000
+// // // console.log(hundreds.toLocaleString('en-IN'))
 
-// console.log(Math)
-// console.log(Math.abs(-4))
-// console.log(Math.round(4.6))
+// // console.log(Math)
+// // console.log(Math.abs(-4))
+// // console.log(Math.round(4.6))
 
-// let myDate = new Date()
-// console.log(myDate.toString())
-// console.log(myDate.toDateString())
-// console.log(myDate.toTimeString())
-// console.log(myDate.toLocaleDateString())
-// console.log(myDate.toLocaleTimeString())
-// console.log(myDate.getFullYear())
-// console.log(myDate.getMonth)
+// // let myDate = new Date()
+// // console.log(myDate.toString())
+// // console.log(myDate.toDateString())
+// // console.log(myDate.toTimeString())
+// // console.log(myDate.toLocaleDateString())
+// // console.log(myDate.toLocaleTimeString())
+// // console.log(myDate.getFullYear())
+// // console.log(myDate.getMonth)
 
-// let myCreateDate = new Date(2023,0,23)
-// console.log(myCreateDate.toDateString())
-// console.log(myCreateDate.toLocaleString())
+// // let myCreateDate = new Date(2023,0,23)
+// // console.log(myCreateDate.toDateString())
+// // console.log(myCreateDate.toLocaleString())
 
+// //        +++++++++array++++++
+
+// const marvel_hero=["thor","ironman","spiderman"];
+// const dc=["superman","flash","batman"];
+
+// // marvel_hero.push(dc)
+// // console.log(marvel_hero)
+
+// // const allHero= marvel_hero.concat(dc)
+// // console.log(allHero)
+
+// const all_new_heros=[...marvel_hero,...dc]
+// console.log(all_new_heros)
+
+console.log(Array.isArray([1,1]))

9f038660bbc2bd64dbd6ec7cf0fa0854a2082d61
kashif5500 <145431430+kashif5500@users.noreply.github.com>
2024-05-15 07:15:07 +0000

    ok

---
+++ 01_basic/03js.js
@@ -0,0 +1,35 @@
+// // const score = 400
+
+// // const balance = new Number(100)
+// // console.log(score);
+
+// // console.log(balance);
+
+// // console.log(balance.toString().length);
+
+// // console.log(balance.toFixed(1));
+
+// // const otherNumber =2667.87665
+
+// // console.log(otherNumber.toPrecision(3));
+
+// // const hundreds=10000000
+// // console.log(hundreds.toLocaleString('en-IN'))
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.6))
+
+// let myDate = new Date()
+// console.log(myDate.toString())
+// console.log(myDate.toDateString())
+// console.log(myDate.toTimeString())
+// console.log(myDate.toLocaleDateString())
+// console.log(myDate.toLocaleTimeString())
+// console.log(myDate.getFullYear())
+// console.log(myDate.getMonth)
+
+// let myCreateDate = new Date(2023,0,23)
+// console.log(myCreateDate.toDateString())
+// console.log(myCreateDate.toLocaleString())
+

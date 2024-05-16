ae5eafb8ed5fea116230fe7639896877d427dc86
kashif5500 <145431430+kashif5500@users.noreply.github.com>
2024-05-16 06:45:36 +0000

    kk

---
+++ 01_basic/04js.js
@@ -0,0 +1,11 @@
+const jsuser = {
+    name:"kashif",
+    email:"kashif@gmail.com",
+    password:"123",
+    city:"jaipur"
+    // isLoggedIn : false;
+    // lastLoginDays :["monadys","saturday"]
+}
+
+console.log(jsuser.email);
+console.log(jsuser["email"])

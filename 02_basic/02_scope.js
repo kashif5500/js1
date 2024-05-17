5af27dc27a14243cbf23fe3cc1da09d1927e2d13
kashif5500 <145431430+kashif5500@users.noreply.github.com>
2024-05-17 19:50:37 +0000

    scope

--- 02_basic/02_scope.js
+++ 02_basic/02_scope.js
@@ -10,3 +10,41 @@ if(true){
 // console.log(a)  
 // console.log(b)
 console.log(c)
+
+function one(){
+    const username ="kashif"
+    function two(){
+        const website ="youtube"
+        console.log(username)
+    }
+    // console.log(website)
+    // two()
+}
+// one()
+
+if(true){
+    const username ="kashif"
+    if(username ==="kashif")
+    {
+        const website =" youtube"
+        // console.log(username + website)
+    }
+    // console.log(website)
+    }
+
+// console.log(username) 
+
+// ++++++++++++++++++ interesting ++++++++++++++++
+
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+
+
+console.log(addtwo(5))
+ const addtwo =function(num){
+
+    return +2
+ }
+

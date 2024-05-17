45fc025b982e2aef2777ee8f8ed143778a6aa3a1
kashif5500 <145431430+kashif5500@users.noreply.github.com>
2024-05-17 17:05:55 +0000

    functin

--- 02_basic/01_function.js
+++ 02_basic/01_function.js
@@ -18,4 +18,20 @@ return `${username} just logged in`
 
 }
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+function calculateCartprice(val1,val2,...num1)
+{
+    return num1            
+     }    
+    //  console.log(calculateCartprice(1000,200,2300,400,5999,3000))   
+    
+const user={
+    username:"kashif",
+    prices: 50000000
+}
+function handleobject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+handleobject(user)

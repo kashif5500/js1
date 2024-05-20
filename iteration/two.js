73343bbbed71418eb3cb821bd297c4f9c10b7bdd
kashif5500 <145431430+kashif5500@users.noreply.github.com>
2024-05-20 17:35:01 +0000

    kk

---
+++ iteration/two.js
@@ -0,0 +1,11 @@
+const arr=[1,2,3,4,5]
+
+for(const num of arr){
+    // console.log(num);
+
+}
+
+const greeting = "Hello world!"
+for (const greet of greeting){
+    console.log(`Each char is ${greet}`);
+}

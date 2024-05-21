af79edb4f4950302715720db47ce73af3a67b5e3
kashif5500 <145431430+kashif5500@users.noreply.github.com>
2024-05-21 15:43:45 +0000

    kk

--- iteration/three.js
+++ iteration/three.js
@@ -23,6 +23,25 @@
 //     console.log(val);
 // })
 
-const coding = ["js","rb","py","java","cpp"]
+// const coding = ["js","rb","py","java","cpp"]
 
-const values= coding.forEach(item)=>
+// const values= coding.forEach((item)=>{
+//     console.log(item);
+//     return item;
+// })
+
+// console.log(values);
+
+const myNums=[1,2,3,4,5,6,7,8,9,10]
+// let x=myNums.filter((num)=>{return num>5})
+// console.log(x);
+
+const newNums=[]
+
+myNums.forEach((num)=>{
+    if (num>4){
+        newNums.push(num)
+    }
+})
+
+console.log(newNums);

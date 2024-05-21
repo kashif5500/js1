3996386a8b4fdc065778ee618243fbff5ebf7216
kashif5500 <145431430+kashif5500@users.noreply.github.com>
2024-05-21 10:17:11 +0000

    kk

---
+++ iteration/three.js
@@ -0,0 +1,28 @@
+// const myObject={
+//     js:'javascript',
+//     cpp:'c++',
+//     rb:'ruby',
+//     swift:'swift by apple'
+    
+// }
+
+// for(const key in myObject){
+//     console.log(`${key} shortcut is for ${myObject[key]}`);
+// }
+
+// const programming =["js","rb","py","java","cpp"]
+
+// for(const key in programming)
+//     {
+//     console.log(key);
+// }
+
+// const coding = ["js","rb","py","java","cpp"]
+
+// coding.forEach(function(val){
+//     console.log(val);
+// })
+
+const coding = ["js","rb","py","java","cpp"]
+
+const values= coding.forEach(item)=>
--- iteration/two.js
+++ iteration/two.js
@@ -1,11 +1,35 @@
-const arr=[1,2,3,4,5]
+// const arr=[1,2,3,4,5]
 
-for(const num of arr){
-    // console.log(num);
+// for(const num of arr){
+//     // console.log(num);
 
+// }
+
+// const greeting = "Hello world!"
+// for (const greet of greeting){
+//     console.log(`Each char is ${greet}`);
+// }
+
+// *** map***
+
+const map= new Map();
+map.set('IN',"India")
+map.set('US',"United States")
+map.set('UK',"United Kingdom")
+map.set('US',"United States")
+
+console.log(map)
+
+for (const [key,value] of map){
+    console.log(key,value);
 }
 
-const greeting = "Hello world!"
-for (const greet of greeting){
-    console.log(`Each char is ${greet}`);
+const myObject={
+    game:'NFS'
+    game2:'spiderman'
 }
+
+// for (const [key,value] of Object.entries(myObject)){
+//     console.log(key,value);
+// }
+
